555ae94e20690c22bcf16a82d2eb1ffda10db3e0
mohamadmahdisamadi <mmsamadi17@gmail.com>
2025-08-08 23:53:21 +0330

    work with LinQ

---
+++ 9.LinQ/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+/*
+    This query groups employees by their department and projects department names alongside with their average salary,
+    at the end it sorts the result based on that average and displays a report of each department.
+*/
+
+List<Employee> employees = new List<Employee>();
+int id = 1;
+employees.Add(new Employee { Id = id++, Name = "Daryl", Department = "OpticsAndDesign", Salary = 10});
+employees.Add(new Employee { Id = id++, Name = "Rick", Department = "OpticsAndDesign", Salary = 9});
+employees.Add(new Employee { Id = id++, Name = "Negan", Department = "OpticsAndDesign", Salary = 8});
+employees.Add(new Employee { Id = id++, Name = "Maggie", Department = "MacrodataRefinement", Salary = 6});
+employees.Add(new Employee { Id = id++, Name = "Carol", Department = "MacrodataRefinement", Salary = 7});
+employees.Add(new Employee { Id = id++, Name = "Carl", Department = "MacrodataRefinement", Salary = 3});
+employees.Add(new Employee { Id = id++, Name = "Rosita", Department = "MacrodataRefinement", Salary = 4});
+employees.Add(new Employee { Id = id++, Name = "Shane", Department = "WellnessCenter", Salary = 5});
+employees.Add(new Employee { Id = id++, Name = "Judith", Department = "WellnessCenter", Salary = 1});
+employees.Add(new Employee { Id = id++, Name = "Eugene", Department = "WellnessCenter", Salary = 2});
+
+var report = employees
+        .Where(e => true)
+        .GroupBy(e => e.Department)
+        .Select(g => new { DepartmentName = g.Key, AvgSalary = g.Average(e => e.Salary), Count = g.Count()})
+        .OrderByDescending(g => g.AvgSalary)
+        .ToList();
+
+foreach (var item in report)
+{
+    Console.WriteLine($"Department {item.DepartmentName} with {item.Count} employees, Has an average salary of {item.AvgSalary}.");
+}
+
+public class Employee
+{
+    public int Id { get; init; }
+    public string Name { get; init; } = string.Empty;
+    public string Department { get; init; } = string.Empty;
+    public double Salary { get; init; }
+}
