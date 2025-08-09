0aeebbd1a6018cc5bb26ffafe824bdd9e2f3d915
mohamadmahdisamadi <mmsamadi17@gmail.com>
2025-08-09 14:53:21 +0330

    still organizing

---
+++ BasicConcepts/5.OOP/Program.cs
@@ -0,0 +1,90 @@
+﻿
+using System;
+
+/*
+    Designing a "Payment Processing System" using OOP concepts such as inheritance and polymorphism.
+    Through this assignment, i learned about properties of a class, abstract classes and interfaces.
+*/
+
+// Test Program
+List<IPaymentMethod> paymentMethods = new List<IPaymentMethod>();
+try { paymentMethods.Add(new CreditCardPayment("111111111111111")); }
+catch (ArgumentException ae) { Console.WriteLine(ae.Message); }
+try { paymentMethods.Add(new CreditCardPayment("1111111111111111")); }
+catch (ArgumentException ae) { Console.WriteLine(ae.Message); }
+
+try { paymentMethods.Add(new PayPalPayment("leaningcsharpgmail.com")); }
+catch (ArgumentException ae) { Console.WriteLine(ae.Message); }
+try { paymentMethods.Add(new PayPalPayment("leaningcsharp@gmail.com")); }
+catch (ArgumentException ae) { Console.WriteLine(ae.Message); }
+
+foreach (var p in paymentMethods)
+{
+    p.ProcessPayment(100);
+}
+
+
+// Classes
+public class PaymentResult
+{
+    public bool Success { get; init; }
+    public string TransactionId { get; init; } = string.Empty;
+    public string ErrorMessage { get; init; } = string.Empty;
+}
+
+public interface IPaymentMethod
+{
+    PaymentResult ProcessPayment(double amount);
+}
+
+public abstract class PaymentMethod : IPaymentMethod
+{
+    public abstract string Name { get; }
+    public abstract PaymentResult ProcessPayment(double amount);
+}
+
+public class CreditCardPayment : PaymentMethod
+{
+    private readonly string _cardNumber;
+    public override string Name => "CreditCard";
+    public CreditCardPayment(string cardnumber)
+    {
+        if (string.IsNullOrWhiteSpace(cardnumber) || cardnumber.Length != 16)
+        {
+            throw new ArgumentException("Enter exactly 16 digits for card number.");
+        }
+        _cardNumber = cardnumber;
+    }
+
+    public override PaymentResult ProcessPayment(double amount)
+    {
+        double processingFee = amount * 0.02;
+        double totalFee = amount + processingFee;
+        Console.WriteLine($"Processing paymenet of credit card ({_cardNumber}) with number {Name} and amount of {totalFee:C}.");
+        return new PaymentResult { Success = true, TransactionId = Guid.NewGuid().ToString() };
+    }
+}
+
+public class PayPalPayment : PaymentMethod
+{
+    private readonly string _emailAddress;
+    public override string Name => "PayPal";
+    public PayPalPayment(string emailAddress)
+    {
+        if (string.IsNullOrWhiteSpace(emailAddress) || !emailAddress.Contains('@'))
+        {
+            throw new ArgumentException("Enter a valid email address.");
+        }
+        _emailAddress = emailAddress;
+    }
+
+    public override PaymentResult ProcessPayment(double amount)
+    {
+        Console.WriteLine($"Redirecting to PayPal for user {_emailAddress}, total cost: {amount:C}.");
+        return new PaymentResult
+        {
+            Success = true,
+            TransactionId = Guid.NewGuid().ToString("N")
+        };
+    }
+}
---
+++ BasicConcepts/7.Exceptions/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+/*
+    A calculator with only Division operation that catches DivisionByZero exception.
+*/
+
+public class Program {
+    public static void Main() {
+        Calculator calculator = new Calculator();
+        List<ValueTuple<int, int>> i = new List<ValueTuple<int, int>>();
+        i.Add((1, 10));
+        i.Add((0, 0));
+        i.Add((-17, 51));
+        foreach ((int a, int b) in i)
+        {
+            Console.Write($"{a} / {b} = ");
+            try {
+                double result = calculator.Divide(a, b);
+                Console.WriteLine(result);
+            } catch (DivisionByZeroException dze) {
+                Console.WriteLine(dze.Message);
+            } finally {
+                Console.WriteLine("done.\n");
+            }
+        }
+    }
+}
+
+public class DivisionByZeroException : System.Exception {
+    public string Message { get; init; } = "Can't divide a number by zero!";
+    public DivisionByZeroException(string message) {
+        Message = message;
+    }
+    public DivisionByZeroException() {}
+}
+
+public class Calculator {
+    public double Divide(double numerator, double denominator) {
+        if (denominator == 0) {
+            throw new DivisionByZeroException();
+        }
+        return numerator / denominator;
+    }
+}
